e2d649a9c197e3f13f16a4161811faa9ef8e5b27
Arpit Namdev <arpit8392@gmail.com>
2023-06-11 14:13:48 +0530

    Add Radio Groups for Feedback Buttons

--- app/components/FeedbackForm.tsx
+++ app/components/FeedbackForm.tsx
@@ -2,39 +2,51 @@
 
 import { FormEvent, useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { RadioGroup } from '@headlessui/react'
+
+function classNames(...classes: Array<string>): string {
+	return classes.filter(Boolean).join(' ')
+}
 
 const FeedbackForm = () => {
-	const feedbackArray = Array.from({ length: 5 }, (_, i) => i + 1)
+	const starsArray = Array.from({ length: 5 }, (_, i) => i + 1)
 	const [stars, setStars] = useState<number | null>(null)
 	const router = useRouter()
-
-	const handleClick = (value: number) => setStars(value)
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 		if (!stars) return
-		console.log(stars)
-		stars && router.push(`/feedback?stars=${stars}`)
+		router.push(`/feedback?stars=${stars}`)
 	}
 	return (
 		<form
 			onSubmit={handleSubmit}
 			className='flex flex-col gap-y-6 pt-2 md:gap-y-8'>
-			<fieldset className='flex flex-wrap justify-around gap-x-4 md:gap-x-5'>
-				<legend className='sr-only'>What is your feedback?</legend>
-				{feedbackArray.map((item) => (
-					<div
-						key={item}
-						onClick={() => handleClick(item)}
-						className={`flex h-10 w-10 items-center justify-center rounded-full text-sm font-bold  hover:cursor-pointer hover:bg-mediumGrey hover:text-white  md:h-14 md:w-14 md:text-base md:text-base ${
-							stars === item
-								? 'bg-orange text-white'
-								: 'bg-darkBlue text-mediumGrey'
-						}`}>
-						{item}
-					</div>
-				))}
-			</fieldset>
+			<RadioGroup value={stars} onChange={setStars} name='stars'>
+				<RadioGroup.Label as='span' className='sr-only'>
+					Select the rating
+				</RadioGroup.Label>
+				<div className='flex flex-wrap justify-around gap-4 md:gap-5'>
+					{starsArray.map((item) => (
+						<RadioGroup.Option
+							key={item}
+							value={item}
+							className={({ active, checked }) =>
+								classNames(
+									active && checked
+										? 'bg-orange text-white'
+										: 'bg-darkBlue text-mediumGrey',
+									'flex h-10 w-10 items-center justify-center rounded-full text-sm font-bold  hover:cursor-pointer hover:bg-mediumGrey hover:text-white  md:h-14 md:w-14 md:text-base md:text-base'
+								)
+							}>
+							<RadioGroup.Label as='span' className='sr-only'>
+								{item}
+							</RadioGroup.Label>
+							<span>{item}</span>
+						</RadioGroup.Option>
+					))}
+				</div>
+			</RadioGroup>
 
 			<button
 				type='submit'
